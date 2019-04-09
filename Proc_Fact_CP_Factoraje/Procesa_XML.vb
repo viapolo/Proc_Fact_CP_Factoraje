6fe6e2d4388c695c5cbf0afdea756da6a151334f
viapolo <viapolo@finagil.com.mx>
2019-04-09 16:35:30 -0500

    - CFDI Cxp

--- Proc_Fact_CP_Factoraje/Procesa_XML.vb
+++ Proc_Fact_CP_Factoraje/Procesa_XML.vb
@@ -5,14 +5,20 @@ Imports System.Math
 Imports System.WeakReference
 Imports System.Xml
 Imports System.Text
+Imports System
 Module Procesa_XML
     Dim path As String = "\\server-nas\CFDI_CP_Factoraje\"
+    Dim pathCxpA As String = "\\server-nas\Contabilidad CFDI\ARCHIVOS ADD CONTPAQi\CFDI_PROV\ARFIN\Todos\"
+    Dim pathCxpF As String = "\\server-nas\Contabilidad CFDI\ARCHIVOS ADD CONTPAQi\CFDI_PROV\FINAGIL\Todos\"
     'Dim path As String = "C:\Users\vicente-apolonio\Desktop\temp\"
     Sub Main()
         Dim dtF100 As New XML_FactorajeDSTableAdapters.Vw_ChequesDetalleTableAdapter
         Dim dtWebXML As New XML_FactorajeDSTableAdapters.WEB_FacturasXMLTableAdapter
         Dim D As System.IO.DirectoryInfo
         D = New System.IO.DirectoryInfo(path)
+
+        procesaXmlCxpA()
+        procesaXmlCxpF()
 
         For Each Archivo As String In My.Computer.FileSystem.GetFiles(
                                 path,
@@ -123,28 +129,59 @@ Module Procesa_XML
         Dim retorno As String = ""
 
         CFDI = doc.DocumentElement
-        If nodo = "RFCR" Or nodo = "RFCE" Then
+        If nodo = "RFCR" Or nodo = "RFCE" Or nodo = "NombreR" Or nodo = "NombreE" Then
             For Each comprobante As XmlNode In CFDI.ChildNodes
-                If comprobante.Name = "cfdi:Receptor" And nodo = "RFCR" Then
+                If comprobante.Name = "cfdi:Receptor" And (nodo = "RFCR" Or nodo = "NombreR") Then
                     For Each receptor As XmlNode In comprobante.Attributes
                         If receptor.Name = "Rfc" Then
                             retorno = receptor.Value.ToString
                             Return retorno
+                            Exit Function
+                        End If
+                        If receptor.Name = "Nombre" And nodo = "NombreR" Then
+                            retorno = receptor.Value.ToString
+                            Return retorno
+                            Exit Function
                         End If
                     Next
                 End If
-                If comprobante.Name = "cfdi:Emisor" And nodo = "RFCE" Then
+                If comprobante.Name = "cfdi:Emisor" And (nodo = "RFCE" Or nodo = "NombreE") Then
                     For Each receptor As XmlNode In comprobante.Attributes
-                        If receptor.Name = "Rfc" Then
+                        If receptor.Name = "Rfc" And nodo = "RFCE" Then
                             retorno = receptor.Value.ToString
                             Return retorno
+                            Exit Function
+                        End If
+                        If receptor.Name = "Nombre" And nodo = "NombreE" Then
+                            retorno = receptor.Value.ToString
+                            Return retorno
+                            Exit Function
                         End If
                     Next
                 End If
             Next
         End If
 
-        If nodo <> "UUID" Then
+        If nodo = "TIR" Or nodo = "TIT" Then
+            For Each comprobante As XmlNode In CFDI.ChildNodes
+                If comprobante.Name = "cfdi:Impuestos" Then
+                    For Each comprobanteC As XmlNode In comprobante.Attributes
+                        If comprobanteC.Name = "TotalImpuestosTrasladados" And nodo = "TIT" Then
+                            retorno = comprobanteC.Value.ToString
+                            Return retorno
+                            Exit Function
+                        End If
+                        If comprobanteC.Name = "TotalImpuestosRetenidos" And nodo = "TIR" Then
+                            retorno = comprobanteC.Value.ToString
+                            Return retorno
+                            Exit Function
+                        End If
+                    Next
+                End If
+            Next
+        End If
+
+        If nodo <> "UUID" And nodo <> "FechaT" Then
             For Each Comprobante As XmlNode In CFDI.Attributes
                 If Comprobante.Name = "Moneda" And nodo = "Moneda" Then
                     retorno = Comprobante.Value.ToString
@@ -189,7 +226,12 @@ Module Procesa_XML
                 For Each Complemento As XmlNode In Comprobante
                     If Complemento.Name = "tfd:TimbreFiscalDigital" Then
                         For Each atributos As XmlNode In Complemento.Attributes
-                            If atributos.Name = "UUID" Then
+                            If atributos.Name = "UUID" And nodo = "UUID" Then
+                                retorno = atributos.Value.ToString
+                                Return retorno
+                                Exit Function
+                            End If
+                            If atributos.Name = "FechaTimbrado" And nodo = "FechaT" Then
                                 retorno = atributos.Value.ToString
                                 Return retorno
                                 Exit Function
@@ -200,4 +242,68 @@ Module Procesa_XML
             Next
         End If
     End Function
+
+    Public Sub procesaXmlCxpA()
+        Dim dtCxp As New XML_CXPDSTableAdapters.CXP_XmlCfdiTableAdapter
+
+        Dim D As System.IO.DirectoryInfo
+        D = New System.IO.DirectoryInfo(pathCxpA)
+
+        For Each Archivo As String In My.Computer.FileSystem.GetFiles(
+                                pathCxpA,
+                                FileIO.SearchOption.SearchTopLevelOnly,
+                                "*.xml")
+
+            Dim nombre() As String = My.Computer.FileSystem.GetName(Archivo).Split(".")
+
+
+            Dim cadXML As String
+            Dim cadena As StreamReader
+            cadena = New StreamReader(Archivo)
+            cadXML = cadena.ReadToEnd
+            cadena.Close()
+
+            Try
+                If dtCxp.Existe_ScalarQuery(leeXMLF(cadXML, "UUID")).ToString = "NE" Then
+                    dtCxp.Insert(leeXMLF(cadXML, "RFCE"), leeXMLF(cadXML, "RFCR"), CDbl(leeXMLF(cadXML, "Total")), CDbl(leeXMLF(cadXML, "TIR")), CDbl(leeXMLF(cadXML, "TIT")), leeXMLF(cadXML, "UUID"), leeXMLF(cadXML, "NombreE"), leeXMLF(cadXML, "Moneda"), leeXMLF(cadXML, "MetodoPago"), leeXMLF(cadXML, "FormaPago"), CDbl(leeXMLF(cadXML, "TipoCambio")), leeXMLF(cadXML, "TipoDeComprobante"), leeXMLF(cadXML, "Serie"), leeXMLF(cadXML, "Folio"), leeXMLF(cadXML, "Fecha"), leeXMLF(cadXML, "FechaT"))
+                    System.IO.File.Move(Archivo, pathCxpA & "Procesados\" & leeXMLF(cadXML, "UUID") & ".xml")
+                    System.IO.File.Move(pathCxpA & nombre(0) & ".pdf", pathCxpA & "Procesados\" & leeXMLF(cadXML, "UUID") & ".pdf")
+                End If
+            Catch ex As Exception
+            End Try
+        Next
+    End Sub
+
+    Public Sub procesaXmlCxpF()
+        Dim dtCxp As New XML_CXPDSTableAdapters.CXP_XmlCfdiTableAdapter
+
+        Dim D As System.IO.DirectoryInfo
+        D = New System.IO.DirectoryInfo(pathCxpF)
+
+        For Each Archivo As String In My.Computer.FileSystem.GetFiles(
+                                pathCxpF,
+                                FileIO.SearchOption.SearchTopLevelOnly,
+                                "*.xml")
+
+            Dim nombre() As String = My.Computer.FileSystem.GetName(Archivo).Split(".")
+
+
+            Dim cadXML As String
+            Dim cadena As StreamReader
+            cadena = New StreamReader(Archivo)
+            cadXML = cadena.ReadToEnd
+            cadena.Close()
+
+            Try
+                If dtCxp.Existe_ScalarQuery(leeXMLF(cadXML, "UUID")).ToString = "NE" Then
+                    dtCxp.Insert(leeXMLF(cadXML, "RFCE"), leeXMLF(cadXML, "RFCR"), CDbl(leeXMLF(cadXML, "Total")), CDbl(leeXMLF(cadXML, "TIR")), CDbl(leeXMLF(cadXML, "TIT")), leeXMLF(cadXML, "UUID"), leeXMLF(cadXML, "NombreE"), leeXMLF(cadXML, "Moneda"), leeXMLF(cadXML, "MetodoPago"), leeXMLF(cadXML, "FormaPago"), CDbl(leeXMLF(cadXML, "TipoCambio")), leeXMLF(cadXML, "TipoDeComprobante"), leeXMLF(cadXML, "Serie"), leeXMLF(cadXML, "Folio"), leeXMLF(cadXML, "Fecha"), leeXMLF(cadXML, "FechaT"))
+                    System.IO.File.Move(Archivo, pathCxpF & "Procesados\" & leeXMLF(cadXML, "UUID") & ".xml")
+                    System.IO.File.Move(pathCxpF & nombre(0) & ".pdf", pathCxpF & "Procesados\" & leeXMLF(cadXML, "UUID") & ".pdf")
+                End If
+            Catch ex As Exception
+            End Try
+        Next
+    End Sub
+
+
 End Module
